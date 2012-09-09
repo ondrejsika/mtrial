147bd8c9e96224552e89d59ac533f3ac87544b6c
Ondrej Sika <sika.ondrej@gmail.com>
2012-09-09 19:09:10 +0200

    add activate env to manage.py

--- project/manage.py
+++ project/manage.py
@@ -2,7 +2,17 @@
 import os
 import sys
 
+normpath = lambda *args: os.path.normpath(os.path.abspath(os.path.join(*args)))
+
 if __name__ == "__main__":
+    # BEGIN activacte virtualenv
+    activate_this = normpath(__file__, '../../env/bin/activate_this.py')
+    try:
+        execfile(activate_this, dict(__file__=activate_this))
+    except IOError:
+        print "E: virtualenv must be installed to PROJECT_ROOT/env"
+    # END activacte virtualenv
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
     from django.core.management import execute_from_command_line
