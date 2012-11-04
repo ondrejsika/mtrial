6e118c5a3a8f69dcf1d74382a822500d55b5121c
Ondrej Sika <sika.ondrej@gmail.com>
2012-11-04 15:51:50 +0100

    fix djangovoid urls

--- sdv/conf/urls.py
+++ sdv/conf/urls.py
@@ -2,11 +2,11 @@ if DEBUG:
     urlpatterns += patterns('',
         url(r'^static/uploads/(?P<path>.*)$', 'django.views.static.serve', {
             'document_root': MEDIA_ROOT,
-        }),
+        }), 
         url(r'^static/static/(?P<path>.*)$', 'django.views.static.serve', {
             'document_root': STATIC_ROOT,
-        }),
-   )
+        }), 
+   ) 
 
 if ADMIN_URLS:
     from django.contrib import admin
